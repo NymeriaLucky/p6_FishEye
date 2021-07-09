2885ed0e82071217b299c57975c99e7b938c490a
Myriam <my.greder@gmail.com>
2021-07-09 16:29:17 +0200

    Correction tabindex

--- js/Photograph.js
+++ js/Photograph.js
@@ -15,12 +15,12 @@ class Photographer {
     let tagHtml = "";
     for (let i = 0; i < this.tags.length; i++) {
       let tag = this.tags[i];
-      tagHtml += `<span tabindex=0 class="tag-photographe">#${tag}</span>`;
+      tagHtml += `<span tabindex="3" class="tag-photographe">#${tag}</span>`;
     }
 
   /*pour afficher le photograph sur la page photographer-page*/
     return `
-       <a tabindex=0 href="photographer-page.html?id=${this.id}" class="linkPhotographer">
+       <a tabindex="4" href="photographer-page.html?id=${this.id}" class="linkPhotographer">
         <div class="photoIdentite"><img src="medias/photographersIDPhotosb/${this.portrait}" alt="lien vers la page de ${this.name}"></div>
         <h2 class="identite">${this.name}</h2>
         <p class="lieu">${this.city}, ${this.country}</p>
@@ -35,7 +35,7 @@ class Photographer {
     let html = "";
 
     this.tags.forEach((tag) => {
-      html += `<a tabindex=0 href="index.html?tag=${tag}" class="tag-photographe" id="${tag}">#${tag}</a>`;
+      html += `<a tabindex="2" href="index.html?tag=${tag}" class="tag-photographe" id="${tag}">#${tag}</a>`;
     });
 
     document.getElementById("tagProfil").innerHTML = html;
